3f0500439e673a82db6a555a3463824d5601276c
Cody Swann <cody@gunnertech.com>
2015-01-16 21:31:56 -0500

    Initial Commit

---
+++ ThumbWar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ThumbWar
+//
+//  Created by Cody Swann on 1/16/15.
+//  Copyright (c) 2015 Gunner Technology. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
